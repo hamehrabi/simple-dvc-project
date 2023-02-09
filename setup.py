95f7bfc8a6b3c8e71085d124430412b2702ae51e
Hamed Mehrabi <mehrabi.hamed@outlook.com>
2023-02-09 17:03:23 +0000

    First Commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="hamehrabi",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hamehrabi/simple-dvc-project",
+    author_email="mehrabi.hamed@outlook.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    license="GNU",
+    python_requires=">=3.8",
+    install_requires=[
+        'dvc',
+        'dvc[gdrive]',
+        'dvc[s3]',
+        'pandas',
+        'scikit-learn'
+    ]
+)
